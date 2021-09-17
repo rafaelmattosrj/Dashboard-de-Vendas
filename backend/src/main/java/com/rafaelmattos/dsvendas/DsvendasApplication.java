a7228d32d64ede912eb1ca835ac969b36cc0e655
Rafael Mattos <rafaelrj@live.com>
2021-09-17 13:46:41 -0300

    Project created

---
+++ backend/src/main/java/com/rafaelmattos/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmattos.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
