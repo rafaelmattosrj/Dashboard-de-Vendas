3e8992e39d693c436dd793efcdc0d42dfb343dfb
Rafael Mattos <rafaelrj@live.com>
2021-09-20 15:57:45 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/rafael-mattos-moreira-414948197/" target="_blank" rel="noreferrer">Rafael Mattos</a></p>
+                <p className="text-light"><small><strong>Projeto Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
