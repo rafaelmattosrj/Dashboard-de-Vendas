9de39df7f2dde8d8a1ef029dce37d29ca1f596c2
Rafael Mattos <rafaelrj@live.com>
2021-09-21 15:42:05 -0300

    Group by search

--- backend/src/main/java/com/rafaelmattos/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/rafaelmattos/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.rafaelmattos.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rafaelmattos.dsvendas.dto.SaleDTO;
+import com.rafaelmattos.dsvendas.dto.SaleSuccessDTO;
+import com.rafaelmattos.dsvendas.dto.SaleSumDTO;
 import com.rafaelmattos.dsvendas.entities.Sale;
 import com.rafaelmattos.dsvendas.repositories.SaleRepository;
 import com.rafaelmattos.dsvendas.repositories.SellerRepository;
@@ -14,16 +18,28 @@ import com.rafaelmattos.dsvendas.repositories.SellerRepository;
 @Service
 public class SaleService {
 
-		@Autowired
-		private SaleRepository repository;
-		
-		@Autowired
-		private SellerRepository sellerRepository;
-		
-		@Transactional(readOnly = true)
-		public Page<SaleDTO> findAll(Pageable pageable) {
-			sellerRepository.findAll();
-			Page<Sale> result = repository.findAll(pageable);
-			return result.map(x -> new SaleDTO(x));
-		}	
+	@Autowired
+	private SaleRepository repository;
+
+	@Autowired
+	private SellerRepository sellerRepository;
+
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
+
+
 }
