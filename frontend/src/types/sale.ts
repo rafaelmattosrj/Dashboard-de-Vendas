682d9199d100ecd520f1cc50fa1449c58619d7e4
Rafael Mattos <rafaelrj@live.com>
2021-09-30 16:50:00 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,6 +1,13 @@
-import { type } from "os";
+import { type } from "os"
+
 
 export type SaleSum = {
-        "sellerName": string,
-        "sum": number
+        sellerName: string;
+        sum: number;
+}
+
+export type SaleSucess = {
+        sellerName: string;
+        visited: number;
+        deals: number;
 }
